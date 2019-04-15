338d6be7aa8c28ea658f4e19b6006d82cd47b4ba
Jiachen Zhu <jiachen.zhu@prodrive-technologies.com>
2019-04-15 18:17:57 +0800

    tweek some codes

--- fake_laser_scan/scripts/fake_laser_scan.py
+++ fake_laser_scan/scripts/fake_laser_scan.py
@@ -2,7 +2,8 @@
 
 import rospy
 from sensor_msgs.msg import LaserScan
-from math import pi, random
+from math import pi
+import random
 
 MAX_RANGE = 20.0
 OBSTACLE_INTERVAL = 3.0   # new obstacles per ? sec
@@ -10,18 +11,18 @@ OBSTACLE_INTERVAL = 3.0   # new obstacles per ? sec
 
 def random_obstacle():
     obstacle_angle = random.randint(0, 360)
-    obstacle_distance = random.randit(2, 10)
+    obstacle_distance = random.uniform(0.3, 2.5)
     return (obstacle_angle, obstacle_distance)
 
 
 def get_scan(obstacles):
-    ranges = [float('inf') in range(360)]
+    ranges = [float('inf') for i in range(360)]
     for obstacle in obstacles:
         mid = obstacle[0]   # obstacle mid angle
-        left = ((mid+360)-5)%360    # width +/-5 degrees
-        right = (mid + 5) % 360
+        left = mid - 3    # width +/-5 degrees
+        right = mid + 3
         dist = obstacle[1]
-        for angle in range(lef, right+1):
+        for angle in range(left, right):
             ranges[anlge] = dist
     return ranges
 
@@ -29,22 +30,24 @@ def get_scan(obstacles):
 rospy.init_node('fake_laser_scan', anonymous=False)
 scan_pub = rospy.Publisher('scan', LaserScan, queue_size=10)
 
+
+scan = LaserScan()
+scan.header.stamp = rospy.Time.now()
+scan.header.frame_id = 'laser_frame'
+scan.angle_min = 0.0
+scan.angle_max = 2 * pi
+scan.angle_increment = pi / 180.0
+scan.range_min = 0.15
+scan.range_max = MAX_RANGE
+
 rate = rospy.Rate(30)
 next_time = rospy.Time.now()
 
 while not rospy.is_shutdown():
-    scan = LaserScan()
-    scan.header.stamp = rospy.Time.now()
-    scan.header.frame_id = 'laser_frame'
-    scan.angle_min = 0.0
-    scan.angle_max = 2 * pi
-    scan.angle_increment = pi / 180.0
-    scan.range_min = 0.15
-    scan.range_max = MAX_RANGE
-
+    obstacles = [random_obstacle() for i in range(3)]   # random 3 obstacles
     if rospy.Time.now() > next_time:
-        scan.ranges = get_scan(random_obstacle())
-        next_time += 3.0
+        scan.ranges = get_scan(obstacles)
+        next_time += rospy.Duration(3.0)
 
     scan_pub.publish(scan)
     rate.sleep()
